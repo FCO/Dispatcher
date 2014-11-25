da220f215c2b2407be008596a33334efc249c172
Fernando Correa de Oliveira <fernandocorrea@gmail.com>
2014-11-25 11:11:45 -0200

    log

--- dispatcher.js
+++ dispatcher.js
@@ -6,7 +6,13 @@ var _match_order = [
 	"method",
 ];
 
-var Dispatcher= function(port, ip) {
+function debug(level, msg) {
+	if(level <= Dispatcher.logLevel)
+		console.log(msg);
+}
+
+var Dispatcher = function(port, ip) {
+	debug(10, "Dispatcher()");
 	if(port !== undefined)
 		this.port = port;
 	if(ip !== undefined)
@@ -16,45 +22,55 @@ var Dispatcher= function(port, ip) {
 	this.importedTemplates = false;
 };
 
+Dispatcher.logLevel = 5;
+
 Dispatcher.prototype = {
 	port:	8080,
 	ip:	'127.0.0.1',
 	start:	function(port, ip) {
+		debug(10, "start()");
 		var http = require('http');
 		
 		http.createServer(function (req, res) {
-			try {
+		debug(10, "createServer()");
+			//try {
 				this.dispatch(req, res);
-			} catch(err) {
-				this.internalServerErrorHandler(req, res);
-			}
+			//} catch(err) {
+			//	this.internalServerErrorHandler(req, res);
+			//}
 		}.bind(this)).listen(
 			port || this.port,
 			ip || this.ip
 		);
-		console.log("Connect at http://" + (ip || this.ip) + ":" + (port || this.port));
+		debug(2, "Connect at http://" + (ip || this.ip) + ":" + (port || this.port));
 	},
 	importTemplates:	function() {
+		debug(10, "importTemplates()");
 		Dispatcher.importTemplates.call(this);
 		this.importedTemplates = true;
 	},
 	firstRoute:	function() {
+		debug(10, "firstRoute()");
 		var new_route = new Dispatcher.Route(this);
 		this.routes.unshift(new_route);
 		return new_route;
 	},
 	route:	function() {
+		debug(10, "route()");
 		var new_route = new Dispatcher.Route(this);
 		this.routes.push(new_route);
 		return new_route;
 	},
 	match:	function(request) {
+		debug(10, "match()");
 		var route;
 		//console.log(request);
 		var matches = this._prepare_routes(this.routes);
 		_match_order.forEach(function(attr){
+			debug(10, "match() forEach()");
 			var newMatches = [];
 			matches.forEach(function(proute){
+				debug(10, "match() forEach() forEach()");
 				if(proute.match(attr, request)) {
 					newMatches.push(proute);
 				}
@@ -65,6 +81,7 @@ Dispatcher.prototype = {
 	},
 
 	dispatch:	function(request, response) {
+			debug(10, "dispatch()");
 		var matches = this.match(request);
 
 		if(matches.length > 0) {
@@ -77,37 +94,45 @@ Dispatcher.prototype = {
 		return;
 	},
 	_prepare_routes:	function(routes) {
+			debug(10, "_prepare_routes()");
 		var prepared = [];
 		routes.forEach(function(route){
+			debug(10, "_prepare_routes() forEach()");
 			this.push(new Dispatcher.Context(route));
 		}.bind(prepared));
 		return prepared;
 	},
 	getRouteByName:		function(name, data) {
+			debug(10, "getRouteByName()");
 		return new Dispatcher.Context(this.namedRoutes[name], data);
 	},
 
 	internalServerErrorHandler:	function(request, response) {
+			debug(10, "internalServerErrorHandler()");
 		response.writeHead(500, {'Content-Type': 'text/plain'});
 		response.end("500 internal server error");
 	},
 	
 	notFoundHandler:	function(request, response) {
+			debug(10, "notFoundHandler()");
 		response.writeHead(404, {'Content-Type': 'text/plain'});
 		response.end("404 not found");
 	},
 
 	_match_method:	function(request) {
+			debug(10, "_match_method()");
 		//console.log(this.method + " == " + request.method);
 		if(this.method === undefined || this.method.length === 0) return true;
 		return this.method.indexOf(request.method) >= 0;
 	},
 
 	_match_uri:	function(request) {
+			debug(10, "_match_uri()");
 		//console.log(this.uri + " == " + request.url);
 		if(this.uri === undefined || this.uri.length === 0) return true;
 		var found = false;
 		(this.uri || []).forEach(function(uri){
+			debug(10, "_match_uri() forEach()");
 			var data = uriTemplate(uri).fromUri(request.url);
 			if(data !== undefined) {
 				for(var key in data) {
@@ -123,18 +148,21 @@ Dispatcher.prototype = {
 };
 
 Dispatcher.importTemplates = function() {
+			debug(10, "importTemplates()");
 	var Template = require("template");
 	this.template = new Template();
 	this.template.pages("templates/*.tmpl");
 };
 
 Dispatcher.render = function(template, data, cb) {
+			debug(10, "Dispatch.render()");
 	this.template.render(template, data, cb);
 };
 
 Dispatcher.prototype.newRoute = Dispatcher.prototype.route;
 
 Dispatcher.Context = function(route, data) {
+			debug(10, "Context()");
 	this.route	= route;
 	this.router	= route.router;
 	this.stash	= data || {};
@@ -148,6 +176,7 @@ Dispatcher.Context = function(route, data) {
 
 Dispatcher.Context.prototype = {
 	clone:	function(fixedData) {
+			debug(10, "clone()");
 			var data = {};
 			for(var key_s in this.stash) {
 				if(this.stash.hasOwnProperty(key_s))
@@ -160,12 +189,14 @@ Dispatcher.Context.prototype = {
 		return new Dispatcher.Context(this.route, data);
 	},
 	match:	function(attr, request) {
+			debug(10, "match()");
 		var hash	= this.route.toHash();
 		hash.stash	= this.stash;
 		hash.params	= this.params;
 		return this.route.router["_match_" + attr].call(hash, request);
 	},
 	exec:		function(request, response, fixedData) {
+			debug(10, "exec()");
 		var context;
 		if(fixedData !== undefined) {
 			context = this.clone(fixedData);
@@ -180,19 +211,25 @@ Dispatcher.Context.prototype = {
 		context.next_handler();
 	},
 	next_handler:	function(resp) {
+			debug(10, "next_handler()");
 		if(resp !== false) this.handle();
 	},
 	next_render:	function(resp) {
+			debug(10, "next_render()");
 		if(resp !== false) this.render();
 	},
 	request: function(method, uri, data, mapper) {
+			debug(10, "request()");
 		console.log(method, uri, data, mapper);
 		throw "request not implemented yet";
 	},
 	handle: 	function() {
+			debug(10, "handle()");
 		var handler = this._handlers.shift();
+		debug(10, "handler:", handler);
 		if(handler) {
 			setTimeout(function(){
+				debug(10, "handle() setTimeout()");
 				handler.cba(this.next_handler, this)(this._request, this._response);
 			}.bind(this), 0);
 			return;
@@ -201,9 +238,12 @@ Dispatcher.Context.prototype = {
 		this._cb.cba(function(req, res){res.end();}, this).call(this, this._request, this._response);
 	},
 	render:		function() {
+			debug(10, "Dispatcher.Context.render()");
 		var render = this._renderes.shift();
+		debug(10, "render:", render);
 		if(render) {
 			setTimeout(function(){
+				debug(10, "render() setTimeout()");
 				render.cba(this.next_render, this)(this._request, this._response);
 			}.bind(this), 0);
 			return;
@@ -213,17 +253,21 @@ Dispatcher.Context.prototype = {
 };
 
 Dispatcher.Route = function(router) {
+			debug(10, "Route()");
 	this.router = router;
 };
 
 Dispatcher.Route.prototype = {
 	toString:	function() {
+			debug(10, "toString()");
 		return this._method + " -> " + this._uri;
 	},
 	newRoute:	function() {
+			debug(10, "newRoute()");
 		return this.router.newRoute();
 	},
 	toHash:		function() {
+			debug(10, "toHash()");
 		var hash = {};
 		for(var key in this) {
 			if(key.substr(0, 1) === "_") {
@@ -233,11 +277,13 @@ Dispatcher.Route.prototype = {
 		return hash;
 	},
 	_handler:	function(request, response) {
+			debug(10, "_handler()");
 		//console.log("custom handler");
 		response.writeHead(200, {'Content-Type': 'text/plain'});
 		response.end("DEFAULT HANDLER: " + this.route.toString());
 	},
 	render:		function(template, fixedData) {
+			debug(10, "Dispatcher.Route.render()");
 		if(typeof this._handler != typeof [])
 			this._handler = [];
 		if(!this.router.importedTemplates) {
@@ -247,6 +293,7 @@ Dispatcher.Route.prototype = {
 			this._render = [];
 
 		this._render.push(function(request, response){
+			debug(10, "render() func");
 			var data = {};
 			for(var key_fd in fixedData) {
         			if(fixedData.hasOwnProperty(key_fd))
@@ -266,6 +313,7 @@ Dispatcher.Route.prototype = {
 		return this;
 	},
 	request:	function(method, uri, data, mapper) {
+			debug(10, "request()");
 		if(typeof this._handler != typeof [])
 			this._handler = [];
 		this._handler.push(function(request, response){
@@ -274,11 +322,13 @@ Dispatcher.Route.prototype = {
 		return this;
 	},
 	name:		function(name) {
+			debug(10, "name()");
 		this._name = name;
 		this.router.namedRoutes[name] = this;
 		return this;
 	},
 	stash2json:	function(mapper) {
+			debug(10, "stash2json()");
 		if(typeof this._handler != typeof [])
 			this._handler = [];
 		this._handler.push(function(request, response){
