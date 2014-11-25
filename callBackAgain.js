7e1a039058ce7a175c819186ca7d83f1bb05422d
Fernando Correa de Oliveira <fernandocorrea@gmail.com>
2014-11-25 17:02:15 -0200

    Adding onError handler

--- callBackAgain.js
+++ callBackAgain.js
@@ -1,9 +1,19 @@
-Function.prototype.cba = function(cb, obj) {
+Function.prototype.cba = function(cb, obj, onError) {
 	return function() {
 		var func_args = arguments;
 		setTimeout(function(){
-			var ret = this.apply(obj, func_args);
-			if(ret !== undefined) cb.call(obj, ret);
+			try {
+				var ret = this.apply(obj, func_args);
+				if(ret !== undefined) cb.call(obj, ret);
+			} catch(err) {
+				if(onError) {
+					var args = Array.prototype.slice.bind(func_args)();
+					args.unshift(err);
+					onError.apply(obj, args);
+				} else {
+					throw err;
+				}
+			}
 		}.bind(this), 0);
-	}.bind(this);
+	}.bind(this)
 };
