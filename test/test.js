a1a196d664b7a21962b7f6faec73224f88ab03c5
FCO <fernandocorrea@gmail.com>
2014-12-03 21:30:01 -0200

    Fix tests

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var should = require("should");
-var Router = require("..");
+var Router = require("../main.js");
 
 describe("requires" , function(){
 	it("should have the right classes", function(){
