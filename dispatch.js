95104653f7a13675d0dcd71c021e0913b39ff299
Fernando Correa de Oliveira <fernandocorrea@gmail.com>
2014-12-03 17:20:40 -0200

    Add dispatch bin

--- dispatch.js
+++ dispatch.js
@@ -1,10 +1,18 @@
+#!/usr/bin/env node
+var path = require("path");
 var Dispatcher = require("./dispatcher.js");
 
 dispatcher = new Dispatcher();
 
 process.argv.slice(2).forEach(function(module){
-	var mod = require(module);
-	mod(dispatcher);
+	var file = path.resolve(module);
+	console.log("importing " + file);
+	var mod = require(file);
+	if(typeof mod !== typeof [])
+		mod = [ mod ];
+	mod.forEach(function(func) {
+		func(dispatcher);
+	});
 });
 
 dispatcher.start();
