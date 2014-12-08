a7e148ad460d1835ff1f79df67bafb973b61cfff
Fernando Correa de Oliveira <fernandocorrea@gmail.com>
2014-12-08 19:38:28 -0200

    Test if file exists

--- dispatch.js
+++ dispatch.js
@@ -1,17 +1,21 @@
 #!/usr/bin/env node
 var path = require("path");
+var fs = require("fs");
 var Dispatcher = require("./main.js");
 
 dispatcher = new Dispatcher();
 
 process.argv.slice(2).forEach(function(module){
 	var file = path.resolve(module);
-	console.log("importing " + file);
-	var mod = require(file);
-	if(typeof mod !== typeof [])
-		mod = [ mod ];
-	mod.forEach(function(func) {
-		func(dispatcher);
+	fs.exists(file, function(exists) {
+		if(!exists) throw "File '" + file + "' do not exist.";
+		console.log("importing " + file);
+		var mod = require(file);
+		if(typeof mod !== typeof [])
+			mod = [ mod ];
+		mod.forEach(function(func) {
+			func(dispatcher);
+		});
 	});
 });
 
