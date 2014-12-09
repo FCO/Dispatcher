67b2ae0167370ebe72dd8306598b9703deec1cfc
FCO <fernandocorrea@gmail.com>
2014-12-08 22:27:26 -0200

    Adding suport for .json files

--- dispatch.js
+++ dispatch.js
@@ -5,7 +5,20 @@ var Dispatcher = require("./main.js");
 
 dispatcher = new Dispatcher();
 
-process.argv.slice(2).forEach(function(module){
+var procs = process.argv.slice(2);
+next_file();
+
+function next_file() {
+	var proc = procs.shift();
+	if(proc)
+		setImmediate(function(proc){
+			return handle_routes.cba(next_file, this)(proc);
+		}.call(this, proc));
+	console.log("will start");
+	dispatcher.start();
+}
+
+function handle_routes(module) {
 	var file = path.resolve(module);
 	fs.exists(file, function(exists) {
 		if(!exists) {
@@ -13,13 +26,23 @@ process.argv.slice(2).forEach(function(module){
 			process.exit();
 		}
 		console.log("importing " + file);
-		var mod = require(file);
-		if(typeof mod !== typeof [])
-			mod = [ mod ];
-		mod.forEach(function(func) {
-			func(dispatcher);
-		});
+		if(path.extname(file) == ".json") {
+			fs.readFile(file, function(err, data){
+				if(err) {
+					console.error(err);
+					process.exit();
+				}
+				this.route(JSON.parse(data));
+			}.cba(next_file, dispatcher));
+		} else {
+			var mod = require(file);
+			if(typeof mod !== typeof [])
+				mod = [ mod ];
+			mod.forEach(function(func) {
+				func.cba(next_file, this)(dispatcher);
+				//func(dispatcher);
+				//setImmediate(function(){next_file()});
+			});
+		}
 	});
-});
-
-dispatcher.start();
+}
